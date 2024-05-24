8051e733ebab2ec0de76deb06de364ba327ee0b3
Domenico Pagano <dpaganoh@gmail.com>
2024-05-24 13:35:10 -0500

    basic setup

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/* Demo tareas Gulp...
+// ----- x ----- Tareas ----- x -----
+function tarea(done){
+    console.log('--$--$--$---$--$--$--\nDesde mi primer tarea\n--$--$--$---$--$--$--');
+    done();
+}
+
+// ----- x ----- Exports ----- x -----
+exports.tarea = tarea;
+*/
+
+function css(){
+    /*
+        Compilar Sass:
+        1. Identificar archivo.
+        2. Compilar.
+        3. Guardar .css
+    */
+}
+
+exports.css = css;
--- info.md
+++ info.md
@@ -2,3 +2,7 @@
 # Gulp
 
     npm i --save-dev gulp
+
+# SASS + Gulp
+
+    npm i --save-dev sass gulp-sass
