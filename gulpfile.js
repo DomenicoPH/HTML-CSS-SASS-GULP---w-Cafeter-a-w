67d2baf912c248f0f15fe4f80d072d888aec7989
Domenico Pagano <dpaganoh@gmail.com>
2024-05-24 20:04:15 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,29 @@ function tarea(done){
 // ----- x ----- Exports ----- x -----
 exports.tarea = tarea;
 */
+const { src, dest, watch } = require('gulp');
+/*  import from gulp...
+    src = source (ls fuente a compilar. El archivo .scss)
+    dest = destination (el archivo .css de destino)
+*/
+const sass = require('gulp-sass')(require('sass'));
 
-function css(){
+function css(done){     // ---------- Función compiladora
     /*
         Compilar Sass:
         1. Identificar archivo.
         2. Compilar.
         3. Guardar .css
     */
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css') )
+    done();
+}
+
+function dev(){         // ---------- watcher
+    watch('src/scss/app.scss', css)
 }
 
 exports.css = css;
+exports.dev = dev;
